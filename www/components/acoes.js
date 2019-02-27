b76ddc7567bcd4d30b309ef966ee3c12fba1e367
jussimar <jussimar@jussimarleal.com.br>
2019-02-28 03:32:20 +0900

    Alteração: inserindo botões e criando a funcionalidade de inserir valores na input

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -1,2 +1,16 @@
 // This is a JavaScript file
+/*$(document).on('click','#btn7',function(){
+    var valor;
+    valor = $("#display").val();
+    valor += "7";
+   $("#display").val(valor);     
+});*/
+
+$(document).on('click','.botao',function(){
+    var valor;
+    valor = $("#display").val();
+    valor += $(this).val();
+    $("#display").val(valor);
+});
+
 
