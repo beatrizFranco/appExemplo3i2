7435e225c524f11cecbb7a9bf86920b44c6e47a7
jussimar <jussimar@jussimarleal.com.br>
2019-03-14 02:14:06 +0900

    Ações dos botões de calculo

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -6,11 +6,44 @@
    $("#display").val(valor);     
 });*/
 
+var op;
+var valor1;
+var valor2;
+
 $(document).on('click','.botao',function(){
     var valor;
     valor = $("#display").val();
     valor += $(this).val();
     $("#display").val(valor);
 });
+
+$(document).on('click','#btnSomar', function(){
+    valor1 = $("#display").val();
+    op="somar";
+    $("#display").val("");
+});
+
+$(document).on('click','#btnSubtrair', function(){
+    valor1 = $("#display").val();
+    op="subtrair";
+    $("#display").val("");
+});
+
+$(document).on('click','#btnMultiplicar', function(){
+    valor1 = $("#display").val();
+    op="mult";
+    $("#display").val("");
+});
+
+$(document).on('click','#btnDividir', function(){
+    valor1 = $("#display").val();
+    op="dividir";
+    $("#display").val("");
+});
+
+
+
+
+
 
 
